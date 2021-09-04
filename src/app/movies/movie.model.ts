71263271d92221fd6dfdd18f186ccdd5e5cdd918
dilma sae <dilmasae@gmail.com>
2021-09-04 12:10:16 +0200

    add page movies

---
+++ src/app/movies/movie.model.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+    id: number,
+    title: string,
+    original_language: string,
+    overview: string,
+    original_title: string,
+    poster_path: string,
+    release_date: string,
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "30448fdcff5e2e30ca0bf35d962bc5a3",
 };
 
 /*
